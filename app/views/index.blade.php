56abe68eb4162b4271352c71711bb0edc0af9f1d
Taher NSE <tahertechs@gmail.com>
2015-01-12 22:00:29 +0200

    Removing Default Laravel Index Page

---
+++ app/views/index.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ENPM - Erciyes Not Paylasim Merkezi</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="container">
+        <div class="jumbotron">
+            <h2>Hey There</h2>
+            <p>This is awesome website for notes sharing</p>
+        </div>
+    </div>
+
+</body>
+</html>
